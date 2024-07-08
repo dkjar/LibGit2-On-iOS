eec2897db68edee2804b35ab2372bc90962fac2a
dkjar <594710967@qq.com>
2024-07-08 14:13:24 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 		.binaryTarget(
 			name: "libgit2",
 			url: "https://github.com/dkjar/LibGit2-On-iOS/archive/refs/tags/1.3.2.zip",
-			checksum: "595c60283c30c3b0e783836307de3047586adc06bd0433a24734b6ca4fe7c015"
+			checksum: "64c8d21b1e816042b61309b2d5aa774113b40c444804dc4b2b59270e00a6afca"
 		),
 	]
 )
