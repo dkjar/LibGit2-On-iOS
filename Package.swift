70d49b99e87833f3e243dab4234f63e9f9d474ac
dkjar <594710967@qq.com>
2024-07-08 15:17:29 +0800

    Update 5 Package.swift

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "libgit2",
-			url: "https://github.com/dkjar/LibGit2-On-iOS/releases/download/1.3.2/libgit2.xcframework.zip",
-			checksum: "595c60283c30c3b0e783836307de3047586adc06bd0433a24734b6ca4fe7c015"
+			url: "https://github.com/dkjar/LibGit2-On-iOS/releases/download/1.3.3/libgit2.xcframework.zip",
+			checksum: "e3be6dfce7418f5d8ac6279f38db4705325d3e4c5557e9e90b33f8c3800313a8"
 		),
 	]
 )
